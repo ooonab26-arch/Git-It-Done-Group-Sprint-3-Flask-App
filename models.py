0550bffc84029a7ef389ac70ccb5a2558ae5e983
pralfredo <132622261+pralfredo@users.noreply.github.com>
2025-11-21 23:43:28 -0500

    Added google signin

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash  # <-- add this
 
 db = SQLAlchemy()
 
@@ -9,8 +10,18 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
-    position = db.Column(db.String(200), nullable=False)
-    # events = db.relationship('Events', backref='user', lazy=True)
+    position = db.Column(db.String(200), nullable=False, default="Staff")
+
+    password_hash = db.Column(db.String(200))
+    google_id = db.Column(db.String(200))
+
+    def set_password(self, password: str):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        if not self.password_hash:
+            return False
+        return check_password_hash(self.password_hash, password)
 
 class Events(db.Model):
     __tablename__ = "events"
