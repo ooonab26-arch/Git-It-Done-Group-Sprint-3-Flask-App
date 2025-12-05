fbb5a108419e77052bb2935bb6f9d5c184edf22e
AOSEI1001 <abisaamankwah51@yahoo.com>
2025-12-04 19:52:56 -0500

    addition of file upload feature and event pop-up on click feature

--- static/js/eventScript.js
+++ static/js/eventScript.js
@@ -16,4 +16,45 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+
+
+
+    const popupWindow = document.createElement("div");
+    popupWindow.className = "event-card-popup";
+
+    document.body.appendChild(popupWindow);
+
+    let currentVisible = false;
+    document.querySelectorAll(".event-row").forEach(row => {
+        row.addEventListener("click", (e) => {
+            e.stopPropagation();
+
+
+            const eventData = JSON.parse(row.dataset.event);
+
+            console.log("Clicked event data:", eventData);
+
+            popupWindow.innerHTML = `<img src="${eventData.poster}" alt="${eventData.title}">
+                <div class="card-body">
+                    <strong>${eventData.title}</strong>
+                </div>`;
+
+            popupWindow.classList.add("show");
+            currentVisible = true;
+            
+        
+    });
+
+});
+
+    document.addEventListener("click", (e) => {
+        if (currentVisible && !e.target.closest(".event-row") && !e.target.closest(".event-card-popup")){
+                    popupWindow.classList.remove("show");
+                    currentVisible = false;
+
+
+        }
+
+
+    });
 });
