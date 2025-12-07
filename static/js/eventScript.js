621b3a75754a99aa841eff4eb625ab3aebe78ebb
AOSEI1001 <abisaamankwah51@yahoo.com>
2025-12-07 03:05:18 -0500

    fixed bugs with event page buttons and event pop up, addition of share links

--- static/js/eventScript.js
+++ static/js/eventScript.js
@@ -1,60 +1,89 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-    document.querySelectorAll('.delete-btn').forEach(btn => {
-        btn.addEventListener('click', () => {
-
-        const id = btn.dataset.id;
-
-        fetch(`/api/v1/events/${id}`, {method: 'DELETE'})
-            .then(response => {
-                if (response.ok){
-                    document.getElementById(`event-${id}`).remove();
-
-                }
-            });
-
-        });
-    });
-
-
-
-
-    const popupWindow = document.createElement("div");
-    popupWindow.className = "event-card-popup";
-
-    document.body.appendChild(popupWindow);
-
-    let currentVisible = false;
-    document.querySelectorAll(".event-row").forEach(row => {
-        row.addEventListener("click", (e) => {
+document.addEventListener("DOMContentLoaded", () => { 
+    document.querySelectorAll('.delete-btn').forEach(btn => { 
+        btn.addEventListener('click', (e) => { 
             e.stopPropagation();
-
-
-            const eventData = JSON.parse(row.dataset.event);
-
-            console.log("Clicked event data:", eventData);
-
-            popupWindow.innerHTML = `<img src="${eventData.poster}" alt="${eventData.title}">
-                <div class="card-body">
-                    <strong>${eventData.title}</strong>
-                </div>`;
-
-            popupWindow.classList.add("show");
-            currentVisible = true;
+            const id = btn.dataset.id; 
             
+            fetch(`/api/v1/events/${id}`, {method: 'DELETE'}) 
+                .then(response => { 
+                    if (response.ok){ 
+                        document.getElementById(`event-${id}`).remove(); 
+                    } 
+                }); 
+            }); 
+        }); 
         
+        const popupWindow = document.createElement("div"); 
+        popupWindow.className = "event-card-popup"; 
+        document.body.appendChild(popupWindow); 
+        
+        let currentVisible = false;
+
+        document.querySelectorAll(".event-info").forEach(row => {
+            row.addEventListener("click", (e) => { 
+                e.stopPropagation(); 
+
+                const eventData = JSON.parse(row.dataset.event); 
+
+                console.log("Clicked event data:", eventData); 
+
+                popupWindow.innerHTML = 
+                    `<img src="${eventData.poster}" alt="${eventData.title}">
+                     <div><strong>${eventData.title}</strong></div>
+
+                    <div class="social-share-container">
+                        <a class="linkedin" target="_blank">
+                         <i class="fa fa-linkedin-square"></i> 
+                        </a>
+                        
+                        <a class="gmail" target="_blank">
+                            <i class="fa fa-google"></i> 
+                        </a> 
+                        
+                        <a class="copy-link" target="_blank">
+                            <i class="fa fa-link"></i>
+                        </a>
+                        
+                        <a class="download" target="_blank">
+                            <i class="fa fa-download"></i> 
+                        </a> <a class="calendar" target="_blank"> 
+                            <i class="fa fa-calendar"></i> 
+                        </a> </div>` ; 
+                        
+                const eventURL = "https://www.colby.edu/now/"; 
+                const eventTitle = encodeURIComponent(eventData.title); 
+
+                const linkedInBtn = popupWindow.querySelector(".linkedin"); 
+                linkedInBtn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(eventURL)}`;
+                linkedInBtn.target = "_blank";
+
+                popupWindow.querySelector(".gmail").href = `https://mail.google.com/mail/?view=cm&fs=1&to=&su=${eventTitle}&body=${encodeURIComponent(eventURL)}`;
+                popupWindow.querySelector(".calendar").href = `https://calendar.google.com/calendar/r/eventedit?text=${eventTitle}&dates=20251206T090000Z/20251206T100000Z&details=${encodeURIComponent(eventURL)}&sf=true&output=xml`;
+                
+                popupWindow.querySelector(".social-share-container")
+                    .addEventListener('click', e => e.stopPropagation());
+                
+                popupWindow.querySelectorAll(".social-share-container a")
+                    .forEach( btn => 
+                        btn.addEventListener("click", e => 
+                            e.stopPropagation()
+                        )
+                    );
+
+                popupWindow.classList.add("show"); 
+                currentVisible = true;
+            });
+        });
+
+        
+        popupWindow.addEventListener("click", e => 
+            e.stopPropagation()
+        );
+        
+        document.addEventListener("click", (e) => { 
+            if (currentVisible && !e.target.closest(".event-info") && !e.target.closest(".event-card-popup")){ 
+                popupWindow.classList.remove("show");
+                currentVisible = false; 
+            } 
+        }); 
     });
-
-});
-
-    document.addEventListener("click", (e) => {
-        if (currentVisible && !e.target.closest(".event-row") && !e.target.closest(".event-card-popup")){
-                    popupWindow.classList.remove("show");
-                    currentVisible = false;
-
-
-        }
-
-
-    });
-});
